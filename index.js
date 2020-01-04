35d15b2dc6ab0ac4ae0d753b38865a448488fe2d
zebulon <zeb64devops@gmail.com>
2020-01-04 04:17:41 +0100

    Modification index.html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Ozimandias le comédien  watchmen!');
+  res.send('Ozimandias le comédien : watchmen!');
 });
 
 var server = app.listen(3000, function () {
